ceea3fc6fbe4dd67b6ec419971261d04ac300d59
Fitsum <Fitsum@gmail.com>
2018-05-17 15:27:17 +0300

    Sith Version

---
+++ demo.java
@@ -0,0 +1,14 @@
+public class demo 
+{
+public static void main()
+{
+  System.out.println("Hello Git ...");
+  System.out.println("Hello Git ...");
+  System.out.println("Hello Git ...");
+  System.out.println("Hello Git ...");
+  System.out.println("Hello Git ...");
+
+}
+
+
+}
