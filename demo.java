e14b7ad1649f8b43236ec2531cd88abb4953fa07
Fitsum <Fitsum@gmail.com>
2018-05-17 14:30:06 +0300

    The Fourth version

--- demo.java
+++ demo.java
@@ -3,8 +3,8 @@ public class demo
 public static void main()
 {
   System.out.println("Hello Git ...");
-System.out.println("Hello Git ...");
-System.out.println("Hello Git ...");
+  System.out.println("Hello Git ...");
+  System.out.println("Hello Git ...");
 
 }
 
